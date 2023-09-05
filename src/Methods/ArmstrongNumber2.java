38d1cba3c02415b704772ab5f4651719a61d87b3
Suresh <2111cs030133@mallareddyuniversity.ac.in>
2023-09-05 22:51:34 +0530

    Day 1

---
+++ src/Methods/ArmstrongNumber2.java
@@ -0,0 +1,30 @@
+package Methods;
+
+import java.util.Scanner;
+// print 3 digit armstrong number
+public class ArmstrongNumber2 {
+
+    public static void main(String[] args) {
+
+        for(int i=100;i<=1000;i++){
+            if (isArmstrong(i)){
+                System.out.println(i);
+            }
+        }
+
+
+    }
+
+    static boolean isArmstrong(int n){
+
+        int original = n,sum=0;
+        while (n>0){
+            int rem = n % 10;
+            sum = sum + rem*rem*rem;
+            n = n/10;
+        }
+
+        return sum==original;
+    }
+
+}
