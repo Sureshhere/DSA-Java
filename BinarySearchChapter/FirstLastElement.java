286b322bdfe1589ec48e0b0d54e202be1feec1e5
Suresh <2111cs030133@mallareddyuniversity.ac.in>
2024-01-29 19:55:42 +0530

    First last element in sorted array

---
+++ BinarySearchChapter/FirstLastElement.java
@@ -0,0 +1,49 @@
+package BinarySearchChapter;
+
+// INPUT : [5,7,7,8,8,10,] target = 8
+// OUTPUT : [3,4]
+
+import java.util.Arrays;
+
+public class FirstLastElement {
+    public static void main(String[] args) {
+        int[] arr = {5,7,7,8,8,10};
+        FirstLastElement a = new FirstLastElement();
+        System.out.println(Arrays.toString(a.searchFirstLast(arr,8)));
+
+    }
+
+    public int[] searchFirstLast(int[] arr,int target){
+        int s=0,e=arr.length-1;
+        int[] ans = {-1,-1};
+
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(arr[mid]==target){
+                ans[0]=mid;
+                e=mid-1;
+            } else if (arr[mid]<target) {
+                s=mid+1;
+            }
+            else {
+                e=mid-1;
+            }
+        }
+        s = 0;
+        e = arr.length-1;
+        
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(arr[mid]==target){
+                ans[1]=mid;
+                s=mid+1;
+            } else if (arr[mid]<target) {
+                s=mid+1;
+            }
+            else {
+                e=mid-1;
+            }
+        }
+        return ans;
+    }
+}
